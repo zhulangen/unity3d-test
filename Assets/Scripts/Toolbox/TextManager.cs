3320bd263735d269e2f274ab386b9b1776e6506b
zhulangren <zhulangern@gmail.com>
2015-05-16 23:25:10 +0800

    添加数据读取单例

---
+++ Assets/Scripts/Toolbox/TextManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.Sqlite;
+namespace Toolbox
+{
+    public class TextManager : XSingleton<TextManager>
+    {
+        //数据库文件储存地址
+        string appDBPath = Application.streamingAssetsPath + "/data.db";
+        DbAccess db;
+        public TextManager()
+        {
+            db = new DbAccess(@"Data Source=" + appDBPath);
+            Debug.Log("yyyyyyyyyyyyyyuuuuuuuuuuuuuu");
+        }
+
+        public string GetWord(int id)
+        {
+            string txt = "";
+            using (SqliteDataReader sqReader = db.SelectWhere("config", new string[] { "txt" }, new string[] { "id" }, new string[] { "=" }, new string[] { id.ToString() }))
+            {
+                while (sqReader.Read())
+                {
+                    txt = sqReader.GetString(sqReader.GetOrdinal("txt"));
+                }
+                sqReader.Close();
+            }
+            return txt;
+        }
+
+    }
+}
