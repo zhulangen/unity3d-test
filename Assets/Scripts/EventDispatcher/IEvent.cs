df5a3c636e3ab03debfc66abadf206ba478b5538
zhulangren <zhulangern@gmail.com>
2015-07-19 23:00:45 +0800

    添加事件系统

---
+++ Assets/Scripts/EventDispatcher/IEvent.cs
@@ -0,0 +1,24 @@
+﻿namespace EventDispatcher
+{
+    /// <summary>
+    /// IEvent接口
+    /// </summary>
+    public interface IEvent
+    {
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>
+        /// type类型.
+        /// </value>
+        string type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the target.
+        /// </summary>
+        /// <value>
+        /// target目标.
+        /// </value>
+        object target { get; set; }
+    }
+}
--- Assets/Scripts/test/test.cs
+++ Assets/Scripts/test/test.cs
@@ -5,7 +5,9 @@ public class test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        SmartURL sl = new SmartURL();
+        sl.rawString = "http://www.cnblogs.com/rchen/archive/2005/06/23/179627.html";
+        sl.test();
 	}
 	
 	// Update is called once per frame
